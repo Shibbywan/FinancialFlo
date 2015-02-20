a0e13b63bf7bab85abfbb55d05857e9eb734a3f4
Kevin Saforo <saforok@mymacewan.ca>
2015-02-20 13:24:02 -0700

    commit getData file

--- FinancialFlo_Java/src/pkg_1/GetData.java
+++ FinancialFlo_Java/src/pkg_1/GetData.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg_1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author Kevin
  */
 public class GetData {
+    private final String s1 = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22";
+    private final String s2 = "%22)&env=store://datatables.org/alltableswithkeys";
+    public GetData() {
+
+    }
     
+    private void requestData(String name) throws MalformedURLException, IOException, ParserConfigurationException, SAXException {
+        URL url = new URL(s1 + name + s2);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/xml");
+        
+        InputStream xml = connection.getInputStream();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = (Document) db.parse(xml);
+        
+        doc.get
+        
+    }
 }
--- FinancialFlo_Java/src/pkg_1/Queries.java
+++ FinancialFlo_Java/src/pkg_1/Queries.java
@@ -9,24 +9,9 @@ import java.util.List;
  * @author Kevin
  */
 public class Queries {
-    QuandlConnection q;
 
     public Queries() {
-            
-        this.q = new QuandlConnection("_L_sd9g2X9JSw5oDVNpS");
-        QDataset d1 = this.q.getDataset("PRAGUESE/PX");
-        //get dataset between two sets of dates... don't forget to use this format for the date.
-        QDataset data2 = q.getDatasetBetweenDates("PRAGUESE/PX","2012-01-01","2012-11-26");
-        //get dataset with custom parameters
-        HashMap<String, String> params = new HashMap<>();
-        params.put("trim_start","2012-09-30");
-        QDataset data3 = q.getDatasetWithParams("PRAGUESE/PX");
-        //get Dataset as List matrix
-        List<List<String>> data3Matrix = data3.getMatrix();
-        //get Dataset as String Matrix
-        String[][] data3StringMatrix = data3.getStringMatrix();
-        System.out.println(Arrays.toString(data3StringMatrix));
-        
+
     }
     
 }
